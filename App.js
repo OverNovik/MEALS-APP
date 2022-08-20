6bc248f3833f3f3cacb0bfffe9ff0165a711f403
Aliaxander Novikov <nerevarin11@gmail.com>
2022-08-20 17:35:35 +0300

    add react navigation

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
-export default function App() {
-  return <CategoriesScreen />;
-}
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <ExpoStatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoriesScreen} />
+          <Stack.Screen name="Meals Overview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {},
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,21 +2,26 @@ import { FlatList } from "react-native";
 import CategoryGridTitle from "../components/CategoryGridTitle";
 import { CATEGORIES } from "../data/dummy-data";
 
-const renderCategoryItem = (itemData) => {
-  return (
-    <CategoryGridTitle
-      title={itemData.item.title}
-      color={itemData.item.color}
-    />
-  );
-};
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate("Meals Overview");
+    };
+    return (
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
 
-const CategoriesScreen = () => {
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={renderCategoryItem}
+      numColumns={2}
     />
   );
 };
